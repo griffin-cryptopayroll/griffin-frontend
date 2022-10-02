5d9d4d542ae2f0ca40583aa74ef2da9df7d38af5
Yuheng Chen <ccyyhh97@hotmail.com>
2022-10-02 00:56:00 -0500

    auth page ui

---
+++ pages/auth.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import Button from '../components/Button'
+
+export default function Auth() {
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
+
+    const handleLogin = () => {
+        if (!username) {
+            alert('gotta have that username')
+        }
+        else if (!password) {
+            alert('no password')
+        }
+        else {
+            // TODO authenticate
+        }
+    }
+
+    return (
+        <div className="h-screen w-screen grid grid-row-1 grid-col-1">
+            <div className="place-self-center">
+                <div className='flex justify-center'>
+                    <div className="text-3xl font-semibold select-none mb-8">G R I F F I N</div>
+                </div>
+                <div className="mb-5">
+                    <input
+                        type="text"
+                        placeholder="username"
+                        className="w-96 p-2 rounded bg-stone-100"
+                        value={username}
+                        onChange={({ target }) => { setUsername(target.value) }}
+                    />
+                </div>
+                <div className="mb-5">
+                    <input
+                        type="password"
+                        placeholder="password"
+                        className="w-96 p-2 rounded bg-stone-100"
+                        value={password}
+                        onChange={({ target }) => { setPassword(target.value) }}
+                    />
+                </div>
+                <div className='flex justify-center'>
+                    <Button
+                        label="Login"
+                        onClickHandler={handleLogin}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
