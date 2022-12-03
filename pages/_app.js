8f43703f826464e3f682e7f14e8f8f90033344e6
Yuheng Chen <ccyyhh97@hotmail.com>
2022-12-03 17:20:28 -0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,31 @@ import {
   useRecoilValue,
 } from 'recoil';
 
+import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+
+// 1. Get projectID 
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+// 2. Configure wagmi client
+const chains = [chain.mainnet]
+const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: 'web3Modal', chains }),
+  provider
+})
+
+// 3. Configure modal ethereum client
+const ethereumClient = new EthereumClient(wagmiClient, chains)
+
 function MyApp({ Component, pageProps }) {
+
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <Component {...pageProps} />
+      </WagmiConfig>
     </RecoilRoot>
   )
 }
