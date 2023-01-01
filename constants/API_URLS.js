b8a38e074ccdc532f9e2484c59c981482f01e20a
Yuheng Chen <ccyyhh97@hotmail.com>
2023-01-01 14:32:58 -0600

    api endpoint update 1

--- constants/API_URLS.js
+++ constants/API_URLS.js
@@ -1,14 +1,13 @@
 const API_URLS = {
   ///백엔드 맞추기!!! 수정전
-  GET_PRICE: "/api/v0/price",
+  GET_PRICE: "/price",
   POST_EMPLOYEE: "/employee",
   DELETE_EMPLOYEE: "/employee",
   GET_EMPLOYEE_SINGLE: "/employee/single",
   GET_EMPLOYEE_MULTI: "/employee/multi",
   GET_EMPLOYER: "/employer",
   GET_VERSION: "/version",
-  GET_PRICE: "/api/v0/price",
-  GET_PAYMENT: "api/v0/payment/employee",
+  GET_PAYMENT: "/payment/employee",
   GET_MAINNET: "/projects/mainnets",
   GET_CATEGORY: "/projects/categories",
   GET_PROJECT_ONE: "/projects/",
